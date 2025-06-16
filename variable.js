919379fabf43154f19d9c8d415497ebb20338dea
hussainm9 <135415975+hussainm9@users.noreply.github.com>
2025-06-16 20:55:32 +0530

    let and const

---
+++ variable.js
@@ -0,0 +1,21 @@
+//variable
+let inbox = 100;
+//let
+let inbox1;
+alert(inbox1);//undefined 
+inbox1 = 200;
+let inbox2 = 200;
+inbox2 = 300;
+alert(inbox1 + inbox2);
+//const
+const allemails = 500;
+console.log(allemails);
+//naming
+/* let 1email = 100; */
+let email1 = 500;
+/* let while = 500; */
+/* let all emails = 600; */
+let allEmails = 600;
+
+let _cat = 'meow'
+

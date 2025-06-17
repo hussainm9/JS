6624a4780841a00cb2ffc3c9fb1055b0d27f8edf
hussainm9 <135415975+hussainm9@users.noreply.github.com>
2025-06-17 16:32:59 +0530

    primitive data types

---
+++ dataTypes.js
@@ -0,0 +1,18 @@
+//number
+const day = 6;
+console.log(day);
+//string
+const name = 'acer'
+console.log(name);
+//boolean
+const isChargingAvaliable = true;
+console.log(isChargingAvaliable);
+//undefined
+let month;
+console.log(month);
+//null
+let year = null;
+console.log(null);
+//Symbol
+const test = Symbol()
+console.log(test);

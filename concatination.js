971c15c8675dc536768ec014d06074c19eb3eff7
hussainm9 <135415975+hussainm9@users.noreply.github.com>
2025-06-24 21:20:09 +0530

    Concatenation Summary code

---
+++ concatination.js
@@ -0,0 +1,15 @@
+//number concatenation
+const bank_1 = 400;
+const bank_2 = 500;
+console.log(bank_1 + bank_2)//900
+//string concatenation using + operator
+const greeting = 'Welcome to our website';
+const user = 'ranger'
+//purpose of back slash
+
+console.log(greeting + user);//Welcome to our websiteranger
+const quote = 'Once a wise man said \"Life  it\'s Sad\"    '
+console.log(quote);
+//purpose of template literal
+const greet = `Welcome to our's website ${user}`
+console.log(greet);
